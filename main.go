be24cba338aed4689a8886e219b7b0b514f6b3a2
Hardik Munjal <hardik.munjal@hitachiconsulting.com>
2020-07-27 22:35:18 +0530

    Completed part 1 of exercise.

--- main.go
+++ main.go
@@ -1,9 +1,72 @@
 package main
 
-import(
-  "fmt"
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
 )
 
-func main(){
-  fmt.Println("This is a Go file")
+type problem struct {
+	question string
+	answer   string
+}
+
+func initArguments() *string {
+	csvFilename := flag.String("csv", "questions.csv", "a csv file in the format of 'question,answer'")
+	flag.Parse()
+	return csvFilename
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
+}
+
+func readCsvData(file *os.File) [][]string {
+	result := csv.NewReader(file)
+	lines, err := result.ReadAll()
+	if err != nil {
+		exit(fmt.Sprintf("Unable to read the questions from csv file."))
+	}
+	return lines
+}
+
+func parseCsvData(lines [][]string) []*problem {
+	problemArray := make([]*problem, len(lines))
+	for i, line := range lines {
+		problemArray[i] = &problem{
+			question: line[0],
+			answer:   strings.TrimSpace(line[1]),
+		}
+	}
+	return problemArray
+}
+
+func startQuiz(problemArray []*problem) {
+	var answer string
+	correct := 0
+	for i, p := range problemArray {
+		fmt.Printf("Problem %d: %s is ?\n", i+1, p.question)
+		fmt.Scanf("%s\n", &answer)
+		if answer == p.answer {
+			fmt.Println("Correct Answer!")
+			correct++
+		}
+	}
+	fmt.Printf("You correctly answered %d out of %d questons!\n", correct, len(problemArray))
+}
+
+func main() {
+	csvFilename := initArguments()
+	file, err := os.Open(*csvFilename)
+	if err != nil {
+		exit(fmt.Sprintf("Unable to open file %s", *csvFilename))
+	} else {
+		fmt.Println("Questions Loaded!")
+	}
+	lines := readCsvData(file)
+	problemArray := parseCsvData(lines)
+	startQuiz(problemArray)
 }
